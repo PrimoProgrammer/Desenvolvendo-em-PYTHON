6672959b9eb7d5fb04817dd9c42e6f21310cc4fc
LuanDev&Engineer <luan.sanka16@gmail.com>
2025-11-28 13:43:30 -0300

    Update Desafio017.py
    Adicionando código

--- Desafio017.py
+++ Desafio017.py
@@ -1,3 +1,15 @@
 print('==== Desafio 17 de Módulos ====')
-#Faça um programa que leia o comprimento  do cateto adjacente de um triangulo retangulo. Calcule e mostre o comprimento
-#da hipotenusa.
+print('Faça um programa que leia o comprimento  do cateto adjacente de um triangulo retangulo. Calcule e mostre o comprimento da hipotenusa')
+
+'''sem importação
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+h1 = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {}'.format(h1)) '''
+
+'''Com importação agora'''
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjascente: '))
+hi = hypot(co,ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
