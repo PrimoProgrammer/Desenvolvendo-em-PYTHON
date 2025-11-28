4e66f3ad2289fb3ceae36f30eaade49c75ee7098
LuanDev&Engineer <luan.sanka16@gmail.com>
2025-11-28 14:55:25 -0300

    Update Desafio021.py
    Adicionado código.

--- Desafio021.py
+++ Desafio021.py
@@ -1,3 +1,11 @@
 print('==== Desafio 21 de Módulos ====')
-#Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
+print('Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.')
 
+import pygame
+pygame.init()
+pygame.mixer.music.load('alok021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
+'''A música foi copiada para a pasta junto com o arquivo do código, ao executar o código a musica se iniciará'''
